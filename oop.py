2eeb00efb59a5b1b0d66add72dfe25ba5e765e7a
lokeyvenus <124059265+lokeyvenus@users.noreply.github.com>
2023-05-05 08:39:17 -0400

    Update oop.py

--- oop.py
+++ oop.py
@@ -52,3 +52,26 @@ ____
 |_____ ''')
 
 def __init__(self, word):
+    self.word = word
+    self.letters = ''
+    
+def play():  
+    words = ["quokka", "ferret", "hedgehog"]
+    attempts = 5
+    hangman = Hangman(random.choice(words))
+    for attempt in range(1, attempts + 1):
+        if result == "win":
+            break
+        print("{0} attempts remain".format(attempt))
+    print("The word is " + hangman.word)
+    print("Goodbye.")
+
+        
+
+def ask_letter(self):
+        while True:
+            letter = input("Guess a letter: ")
+            if letter in self.letters:
+                print("This letter is already played, sorry")
+                continue
+        
