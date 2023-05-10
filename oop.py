bc984f3c8e10b8a3b9c6130125ac6c541d14fc4b
lokeyvenus <124059265+lokeyvenus@users.noreply.github.com>
2023-05-10 08:39:11 -0400

    Update oop.py

--- oop.py
+++ oop.py
@@ -1,2 +1,29 @@
-class char
-self__init__
+class char:
+    def __init__(self, name, element):
+        self.name = name
+        self.element = element
+
+class Main(char):
+    def __init__(self, name, element, region, status):
+        super().__init__(name, element)
+        self.region = region
+        self.status = status
+    def show(self):
+        print('Name:', self.name, 'Element:', self.element, 'Region:', self.region, 'Status:', self.status)
+    
+    Lumine = char('Lumine', 'anemo', 'Teyvat', 'main playable protagonist')
+
+class Player_1(Main):
+    def __init__(self, name, element, region, status):
+        super().__init__(name, element, region, status)
+    def show(self):
+        print('Name:', self.name, 'Element:', self.element, 'Region:', self.region, 'Status:', self.status)
+    
+    Chongyun = char('Chongyun', 'cryo', 'Liyue', 'playable four star')
+
+class Player_2(Player_1):
+    def __init__(self, name, element, region, status, privileges):
+        super().__init__(name, element, region, status)
+        self.privileges = privileges
+    def show(self):
+        print('Name:', self.name, 'Element:', self.element, 'Region:', self.region, 'Status:', self.status, 'Privileges', sel)
