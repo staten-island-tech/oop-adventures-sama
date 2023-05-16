439b8ed59f1351bc8202fd38ae976e01088b2f97
lokeyvenus <124059265+lokeyvenus@users.noreply.github.com>
2023-05-16 08:39:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,48 @@
-class char:
-    def __init__(self, name):
-        self.name = name
+print('Welcome Traveler!')
+print('Along with your brother, you are an avid explorer from another dimension.')
+print('You have just set foot on Teyvat.')
+print('After your arrival, however, you encounter an unexpected disaster which causes the separation of you and your brother.')
+print('You have to cross three regions--Monstadt, Liyue, and Inazuma, and find clues about your brother. You may find hilichurls, or monsters, along the way')
+print('Lets start with your name:')
+name = input()
+print('Good luck and may your journey begin.')
 
-class Main(char):
+class Main:
     def __init__(self, name, region, status, weapon):
-        super().__init__(name)
+        self.name = name
         self.region = region
         self.status = status
         self.weapon = weapon
     def __str__(self):
         return f'{self.name}, {self.region}, {self.status}, {self.weapon}'
-    Lumine = ('Lumine, Teyvat, main playable protagonist, sword')
-    print(Lumine)
+    Traveler = ('Traveler, unknown, main playable protagonist, sword')
+    print(Traveler)
 
-class Player_1(Main):
+class NPC_1(Main):
     def __init__(self, name, region, status, weapon, element):
         super().__init__(name, region, status, weapon)
         self.element = element
     def __str__(self):
         return f'{self.name}, {self.region}, {self.status}, {self.weapon}, {self.element}'
-    Chongyun = ('Chongyun, Liyue, playable four star, claymore, cryo/ice')
+    Chongyun = ('Chongyun, Liyue, npc, claymore, cryo/ice')
     print(Chongyun)
 
-class Player_2(Player_1):
+class NPC_2(NPC_1):
     def __init__(self, name, region, status, weapon, element):
         super().__init__(name, element, region, status, weapon)
     def show(self):
         print('Name:', self.name, 'Element:', self.element, 'Region:', self.region, 'Status:', self.status, 'Weapon', self.weapon)
     def __str__(self):
         return f'{self.name}, {self.region}, {self.status}, {self.weapon}, {self.element}'
-    Yoimiya = ('Yoimiya, Inazuma, playable five star, bow, pyro/fire')
+    Yoimiya = ('Yoimiya, Inazuma, npc, bow, pyro/fire')
     print(Yoimiya)
+
+class NPC_3(NPC_2):
+    def __init__(self, name, region, status, weapon, element):
+        super().__init__(name, element, region, status, weapon)
+    def show(self):
+        print('Name:', self.name, 'Element:', self.element, 'Region:', self.region, 'Status:', self.status, 'Weapon', self.weapon)
+    def __str__(self):
+        return f'{self.name}, {self.region}, {self.status}, {self.weapon}, {self.element}'
+    Jean = ('Jean, Monstadt, npc, sword, anemo/wind')
+    print(Jean)
