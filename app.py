17dee8265ca516e3aeae99edc087df31619f87a1
lokeyvenus <124059265+lokeyvenus@users.noreply.github.com>
2023-05-22 08:39:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,22 +4,17 @@ print('Welcome Traveler!')
 print('Along with your brother, you are an avid explorer from another dimension.')
 print('You have just set foot on Teyvat.')
 print('After your arrival, however, you encounter an unexpected disaster which causes the separation of you and your brother.')
-print('You have to cross three regions--Monstadt, Liyue, and Inazuma, and find clues about your brother. You may find hilichurls, or monsters, along the way')
+print('You must complete this journey in order to find your brother. You may find hilichurls, or monsters, along the way.')
 print('Lets start with your name:')
 name = input()
+print('Hello there.')
 
-class Item:
-    def __init__(self, name, rarity):
+class Weapon:
+    def __init__(self, name, rarity, damage, type):
         self.name = name
         self.rarity = rarity
-    def __str__(self):
-        return f'{self.name}, {self.rarity}'
-
-class Weapon(Item):
-    def __init__(self, name, rarity, damage, type):
         self.dmg = damage
         self.type = type
-        super().__init__(name, rarity, damage, type)
     def __str__(self):
         return f'{self.name}, {self.rarity}, {self.dmg}, {self.type}'
 
@@ -35,8 +30,9 @@ class Aqua_Simulacra(Weapon):
     def __init__(self):
         super().__init__(name='Aqua Simulacra', rarity='5 star', damage='44', type='bow')
 
-weapon_list = [Aquila_Favonia, Deathmatch, Aqua_Simulacra]
-print(random.choice(weapon_list))
+weapons = {'Aquila Favonia':Aquila_Favonia, "Deathmatch":Deathmatch, 'Aqua Simulacra':Aqua_Simulacra}
+weapom = random.choice(weapons.values())()
+print(random.choice(weapons))
 print('This is your weapon. You will use it as defense against any barriers along the way.')
 print('Good luck and may your journey begin.')
 
@@ -48,7 +44,7 @@ class Enemy:
 
 class Hilichurl(Enemy):
     def __init__(self):
-        super().__init__(name='Hilichurl', hp='60', damage='15')
+        super().__init__(name='Hilichurl', hp='60', damage='15') 
 
 class Hilichurl_Shooter(Enemy):
     def __init__(self):
