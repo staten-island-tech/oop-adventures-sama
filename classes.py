3bd952fb8aa47f1c347b2fc9cb64b3bf12e763eb
lokeyvenus <124059265+lokeyvenus@users.noreply.github.com>
2023-06-06 08:39:43 -0400

    .

--- classes.py
+++ classes.py
@@ -1,21 +1,13 @@
 import random 
-print('Welcome Traveler!')
-print('Along with your brother, you are an avid explorer from another dimension.')
-print('You have just set foot on Teyvat.')
-print('After your arrival, however, you encounter an unexpected disaster which causes the separation of you and your brother.')
-print('You must complete this journey fight these hilichurls, or monsters, along the way.')
-print('Lets start with your name:')
-name = input()
-print('Good luck and may your journey begin.')
 
 class Weapon:
     def __init__(self, name, rarity, damage, type):
         self.name = name
         self.rarity = rarity
-        self.dmg = damage
+        self.damage = damage
         self.type = type
     def __str__(self):
-        return f'{self.name}, {self.rarity}, {self.dmg}, {self.type}'
+        return f'{self.name}, {self.rarity}, {self.damage}, {self.type}'
 
 class Aquila_Favonia(Weapon):
     def __init__(self):
@@ -33,28 +25,34 @@ class Traveler:
     def __init__(self, name):
         self.name = name
         self.hp = 100
-        self.attack = 0
+        self.damage = 0
         self.weapon = 0
-    def attack(self):
-        self.hp
+    def damage_taken(self, damage_amount):
+        if damage_amount > self.hp:
+            self.hp -= damage_amount
+            self.hp = 0
     def weapon(self):
         weapon = {'Aquila Favonia':Aquila_Favonia, 'Deathmatch':Deathmatch, 'Aqua Simulacra':Aqua_Simulacra}
         print(random.choice(weapon))
     def dodge(self):
-        self.hp += Enemy.attack
+        self.hp += Enemy.damage
         print(f'{self.name} successfully dodged {Enemy.name}\'s attack')
-    def attack(self):
-        Enemy.hp -= self.attack
-        print(f'{self.attack} damage done to {Enemy.name}')
+    def damage(self):
+        Enemy.hp -= self.damage
+        print(f'{self.damage} damage done to {Enemy.name}')
 
 class Enemy:
     def __init__(self, name):
         self.name = name
         self.hp = 70
-        self.attack = 0
-    def attack(self):
-        Traveler.hp -= self.attack
-        print(f'{self.attack} damage done to {Traveler.name}')
+        self.damage = 0
+    def damage_taken(self, damage_amount):
+        if damage_amount > self.hp:
+            self.hp -= damage_amount
+            self.hp = 0
+    def damage(self):
+        Traveler.hp -= self.damage
+        print(f'{self.damage} damage done to {Traveler.name}')
 
 class Hilichurl(Enemy):
     def __init__(self):
@@ -68,16 +66,5 @@ class Unusual_Hilichurl(Enemy):
     def __init__(self):
         super().__init__(name='Unusual Hilichurl', hp='85', damage='20')
                                                                         
-class Game:
 
-    def result(self):
-        if Traveler.hp < 1 and Enemy.hp > 0:
-            self.game_over = True
-            print('Defeated')
-        elif Enemy.hp < 1 and Traveler.hp > 0:
-            self.game_over = True
-            print('Victory!!')
-        elif Traveler.hp < 1 and Enemy.hp < 1:
-            self.game_over = True
-        print('Draw')
         
--- game.py
+++ game.py
@@ -5,10 +5,23 @@ def start_game():
     print('Along with your brother, you are an avid explorer from another dimension.')
     print('You have just set foot on Teyvat.')
     print('After your arrival, however, you encounter an unexpected disaster which causes the separation of you and your brother.')
-    print('You must complete this journey fight these hilichurls, or monsters, along the way.')
-    name = input('Lets start with your name:')
-    name = Traveler(name)
-    print('Good luck and may your journey begin.')
+    print('You must complete this journey fight these hilichurls, or monsters, along the way.')   
+
+while True:
+    print('Do you wish to continue? (yes/no)')
+    user_input = input()
+        
+    if user_input() == 'yes':
+        print('Great!')
+        name = input('Lets start with your name:')
+        name = Traveler(name)
+        print('Good luck and may your journey begin.')
+        start_game = True
+    elif user_input() == 'no':
+        print('Some other time then :((')
+        break
+    else:
+        print('Please type a valid answer.')
 
 game_over = True
 while (game_over is True):
